aac17e704294e8815a1abc3e0cab5fdc783937ca
William Rover <roverwilliam4567@gmail.com>
2025-12-02 23:55:59 +0200

    Update 1.3
    Added href to gallery.html for filepath testing

---
+++ Pages/Gallery/gallery.js
@@ -0,0 +1,5 @@
+import {loadFile} from "./Reuse/shared.js";
+// Load navbar
+loadFile("../../Reuse/Navbar/navbar.html", "navbar");
+// Load footer
+loadFile("../../Reuse/Footer/footer.html", "footer");

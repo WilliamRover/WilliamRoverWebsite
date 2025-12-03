3cdb09f036ef3ec93a5667b5610df28c80ef4e82
William Rover <roverwilliam4567@gmail.com>
2025-12-03 10:35:55 +0200

    Update 1.4
    Fix navbar and footer pathing

--- homepage.js
+++ homepage.js
@@ -7,16 +7,16 @@ loadFile("./Reuse/Footer/footer.html", "footer");
 // Main
 import {checkTime} from "./Reuse/shared.js";
 if (checkTime()) {
-    banner.innerHTML = "<img src=\"./Pages/Homepage/bannerLight.png\">"
+    banner.innerHTML = "<img src=\"./Homepage/bannerLight.png\">"
 } else if (checkTime() == false) {
-    banner.innerHTML = "<img src=\"./Pages/Homepage/bannerDark.png\">"
+    banner.innerHTML = "<img src=\"./Homepage/bannerDark.png\">"
 }
 
 window.onclick = function() {
     var light = window.getComputedStyle(document.body).getPropertyValue('--light');
     if (light == 'true') {
-        banner.innerHTML = "<img src=\"./Pages/Homepage/bannerLight.png\">"
+        banner.innerHTML = "<img src=\"./Homepage/bannerLight.png\">"
     } else if (light == 'false') {
-        banner.innerHTML = "<img src=\"./Pages/Homepage/bannerDark.png\">"
+        banner.innerHTML = "<img src=\"./Homepage/bannerDark.png\">"
     }
 }

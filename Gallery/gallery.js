062d09374edaacbd43f94399cd9647242be427dd
William Rover <roverwilliam4567@gmail.com>
2026-02-05 22:09:18 +0200

    Update 1.9
    Add line seperate between sections.
    Add darker color on selected sort button.
    Fix the class structure to be more dynamic.
    Working on switching between grid and line by line

--- Gallery/gallery.js
+++ Gallery/gallery.js
@@ -1,152 +1,45 @@
 import {loadFile} from "../Reuse/shared.js";
-import {translateData} from "../lang.js";
+
+
 // Load navbar
 loadFile("../Reuse/Navbar/navbar.html", "navbar");
 // Load footer
 loadFile("../Reuse/Footer/footer.html", "footer");
 
-function getNested(obj, path) {
-    return path.split('.').reduce((o, k) => o?.[k], obj);
-}
+
+import {Sort} from "../Reuse/shared.js";
 
 // Sort funcs
-class Sort {
-    #key;
-    #sectionIndex = 0
-    ready
-    constructor(filePath) {
-        this.ready = fetch(filePath).then(response => {
-        return response.json();
-        }).then(file => {
-            this.file = file
-            this.fileLen = Object.keys(file).length
-        })
-        this.#key = [".thumbnail", ".fileName", ".author", ".dateCreated", ".yearCreated", ".type.generalType"]
-    }
-    
-    #addToSection(i, sectionIndex) {
-        var work = document.getElementById("workTemp1")
-        let div = document.createElement("div")
-        div.id = "work".concat(i)
-        div.classList.add("work")
-        // console.log(div.id)
-        div.innerHTML = work.innerHTML
-        // console.log("section".concat(sectionIndex))
-        document.getElementById("section".concat(sectionIndex)).appendChild(div)
-    }
 
-    #assignInfo() {
-        for (let i = 0; i < this.fileLen; i++) {
-            for (let j = 0; j < this.#key.length; j++) {
-                if (j == 5) {
-                    var access = document.querySelector(`#work${i} .type\\.generalType`)
-                } else {
-                    var access = document.querySelector(`#work${i} ${this.#key[j]}`)
-                }
-                var res = getNested(this.file[i], this.#key[j].replace('.', ''))
-                
-                if (j == 3 || j == 5) {
-                    var curLan = localStorage.getItem('selectedLanguage') || 'en';
-                    access.setAttribute("idLan", res)
-                    translateData(curLan)
-                }
-                if (j == 4) {
-                    res = ' '.concat(res)
-                }
-                if (j == 0) {
-                    access.src = res
-                    continue
-                }
-                access.innerHTML = res
-            }
-        }
-    }
-    // In the JSON, the order is already listed from oldest to newest
-    #assignAndCreateWorkDiv(Arr) {
-        var sectionIndex = -1
-        for (let i = 0; i < Arr.length; i++) {
-            if (i % 3 == 0) {
-                sectionIndex ++
-                this.#createSection(sectionIndex)
-            }
-            this.#addToSection(Arr[i], sectionIndex)
-        }
-        this.#assignInfo()
-        this.#sectionIndex = sectionIndex
-        console.log(this.#sectionIndex)
-    }
-    #sortAlphabetically() {
-        var workNameArr = []
-        for (let i = 0; i < this.fileLen; i++) {
-            workNameArr.push(this.file[i]["fileName"])
-        }
-        var workNameSort = workNameArr.toSorted()
-        var workIndexArr = []
-        for (let i = 0; i < workNameSort.length; i++) {
-            for (let j = 0; j < workNameArr.length; j++) {
-                if (workNameArr[j] == workNameSort[i]) {
-                    workIndexArr.push(j)
-                    break
-                }
-            }
-        }
-        return workIndexArr
-    }
-    
-    #createSection(sectionIndex) {
-        let div = document.createElement("div")
-        div.id = "section".concat(sectionIndex)
-        div.classList.add("section")
-        document.getElementById("midpage").appendChild(div)
-    }
-
-    sortOldest() {
-        this.#assignAndCreateWorkDiv(Object.keys(this.file))
-    }
-
-    sortNewest() {
-        this.#assignAndCreateWorkDiv(Object.keys(this.file).reverse())
-    }
-
-
-    sortAtoZ() {
-        var workIndexArr = this.#sortAlphabetically()
-        this.#assignAndCreateWorkDiv(workIndexArr)
-    }
-    
-    sortZtoA() {
-        var workIndexArr = this.#sortAlphabetically().reverse()
-        this.#assignAndCreateWorkDiv(workIndexArr)
-    }
-
-    clearWorkDiv() {
-        for (let i = 0; i <= this.#sectionIndex; i++) {
-            console.log(i)
-            let elem = document.getElementById("section".concat(i))
-            elem.remove()
-        }
-        this.#sectionIndex = 0
-    }
-}
 // fetch(`./Data/gallery.json`).then(response => {
 //         return response.json();
 //         }).then(file => {
 //             new Sort(file).sortNewest()
 //         })
-var workSort = new Sort(`./Data/gallery.json`)
+
+var workSort = new Sort(`./Data/gallery.json`, "workTemp1", "Grid")
 function sortWork(curSort) {
+    changeColor(prevBtn, "#063B7D")
     workSort.ready.then(() => {
         switch (curSort) {
             case "newest":
+                changeColor(newBtn, "#052854")
+                prevBtn = newBtn
                 workSort.sortNewest()
                 break
             case "oldest":
+                changeColor(oldBtn, "#052854")
+                prevBtn = oldBtn
                 workSort.sortOldest()
                 break
             case "alphabetical":
+                changeColor(alphabetBtn, "#052854")
+                prevBtn = alphabetBtn
                 workSort.sortAtoZ()
                 break
             case "alphabeticalRev":
+                changeColor(alphabetRevBtn, "#052854")
+                prevBtn = alphabetRevBtn
                 workSort.sortZtoA()
                 break
             case "clearWork":
@@ -159,12 +52,20 @@ function sortWork(curSort) {
 // // newest, oldest, AtoZ, ZtoA
 var curSort = localStorage.getItem("selectedSort") || "newest"
 // // console.log(curSort)
+var prevBtn;
 sortWork(curSort)
-var newBtn = document.getElementById("newest")
-var oldBtn = document.getElementById("oldest")
-var alphabetBtn = document.getElementById("alphabetical")
-var alphabetRevBtn = document.getElementById("alphabeticalRev")
+var newBtn = document.getElementById("sortBtnNewest")
+var oldBtn = document.getElementById("sortBtnOldest")
+var alphabetBtn = document.getElementById("sortBtnAlphabetical")
+var alphabetRevBtn = document.getElementById("sortBtnAlphabeticalRev")
 
+function changeColor(obj, color) {
+    try {
+        obj.style.backgroundColor = color;
+    } catch (error) {
+        return;
+    }
+}
 newBtn.onclick = function() {
     curSort = "newest"
     localStorage.setItem("selectedSort", curSort);
--- Reuse/shared.js
+++ Reuse/shared.js
@@ -24,3 +24,135 @@ export function checkTime() {
         return false
     }
 }
+
+import {translateData} from "../lang.js";
+function getNested(obj, path) {
+    return path.split('.').reduce((o, k) => o?.[k], obj);
+}
+
+
+export class Sort {
+    #key;
+    #sectionIndex = 0
+    ready
+    constructor(filePath, WorkTempName, sortMode) {
+        this.ready = fetch(filePath).then(response => {
+        return response.json();
+        }).then(file => {
+            this.file = file
+            this.fileLen = Object.keys(file).length
+        })
+        this.#key = [".thumbnail", ".fileName", ".author", ".dateCreated", ".yearCreated", ".type.generalType"]
+        this.WorkTempName = WorkTempName
+        this.sortMode = sortMode
+        // this.sectionTempName = sectionTempName
+    }
+    
+    #addToSection(i, sectionIndex) {
+        var work = document.getElementById(this.WorkTempName)
+        let div = document.createElement("div")
+        div.id = "work".concat(i)
+        div.classList.add("work".concat(this.sortMode))
+        // console.log(div.id)
+        div.innerHTML = work.innerHTML
+        // console.log("section".concat(sectionIndex))
+        document.getElementById("section".concat(sectionIndex)).appendChild(div)
+    }
+
+    #assignInfo() {
+        for (let i = 0; i < this.fileLen; i++) {
+            for (let j = 0; j < this.#key.length; j++) {
+                if (j == 5) {
+                    var access = document.querySelector(`#work${i} .type\\.generalType`)
+                } else {
+                    var access = document.querySelector(`#work${i} ${this.#key[j]}`)
+                }
+                var res = getNested(this.file[i], this.#key[j].replace('.', ''))
+                
+                if (j == 3 || j == 5) {
+                    var curLan = localStorage.getItem('selectedLanguage') || 'en';
+                    access.setAttribute("idLan", res)
+                    translateData(curLan)
+                }
+                if (j == 4) {
+                    res = ' '.concat(res)
+                }
+                if (j == 0) {
+                    access.src = res
+                    continue
+                }
+                access.innerHTML = res
+            }
+        }
+    }
+    // In the JSON, the order is already listed from oldest to newest
+    #assignAndCreateWorkDiv(Arr) {
+        var sectionIndex = -1
+        for (let i = 0; i < Arr.length; i++) {
+            if (i % 3 == 0 && this.sortMode == "Grid") {
+                sectionIndex ++
+                this.#createSection(sectionIndex)
+                    let div = document.createElement("div")
+                    div.id = "sectionLine".concat(sectionIndex)
+                    div.classList.add("sectionLine")
+                    document.getElementById("section".concat(sectionIndex)).appendChild(div)
+            }
+            this.#addToSection(Arr[i], sectionIndex)
+        }
+        this.#assignInfo()
+        this.#sectionIndex = sectionIndex
+        console.log(this.#sectionIndex)
+    }
+    #sortAlphabetically() {
+        var workNameArr = []
+        for (let i = 0; i < this.fileLen; i++) {
+            workNameArr.push(this.file[i]["fileName"])
+        }
+        var workNameSort = workNameArr.toSorted()
+        var workIndexArr = []
+        for (let i = 0; i < workNameSort.length; i++) {
+            for (let j = 0; j < workNameArr.length; j++) {
+                if (workNameArr[j] == workNameSort[i]) {
+                    workIndexArr.push(j)
+                    break
+                }
+            }
+        }
+        return workIndexArr
+    }
+    
+    #createSection(sectionIndex) {
+        let div = document.createElement("div")
+        div.id = "section".concat(sectionIndex)
+        div.classList.add("section")
+        document.getElementById("midpage").appendChild(div)
+    }
+
+    sortOldest() {
+        this.#assignAndCreateWorkDiv(Object.keys(this.file))
+    }
+
+    sortNewest() {
+        this.#assignAndCreateWorkDiv(Object.keys(this.file).reverse())
+    }
+
+
+    sortAtoZ() {
+        var workIndexArr = this.#sortAlphabetically()
+        this.#assignAndCreateWorkDiv(workIndexArr)
+    }
+    
+    sortZtoA() {
+        var workIndexArr = this.#sortAlphabetically().reverse()
+        this.#assignAndCreateWorkDiv(workIndexArr)
+    }
+
+    clearWorkDiv() {
+        for (let i = 0; i <= this.#sectionIndex; i++) {
+            // console.log(i)
+            let elem = document.getElementById("section".concat(i))
+            elem.remove()
+        }
+        this.#sectionIndex = 0
+    }
+}
